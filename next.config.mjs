a666a567087cdb656799ad9b3fa3d49858b32275
R0X4R <32596297+R0X4R@users.noreply.github.com>
2024-10-07 01:03:27 +0530

    fixed nextjs config file
    FIxed a few errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    distDir: "dist",
+    images: { unoptimized: true },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
