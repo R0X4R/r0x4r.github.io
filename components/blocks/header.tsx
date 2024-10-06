bd6f9077a5c181122cd23e981abe5ea1f22f73d7
R0X4R <32596297+R0X4R@users.noreply.github.com>
2024-10-07 01:07:07 +0530

    Update header.tsx

--- components/blocks/header.tsx
+++ components/blocks/header.tsx
@@ -15,7 +15,6 @@ const Header = () => {
         const lenis = new Lenis({
             duration: 1.2,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            direction: "vertical",
             smooth: true,
         })
 
