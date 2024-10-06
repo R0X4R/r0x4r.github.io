097b80c3e8c0b8433991f86de37f3f8db3b530d4
R0X4R <32596297+R0X4R@users.noreply.github.com>
2024-10-07 01:09:55 +0530

    Update header.tsx

--- components/blocks/header.tsx
+++ components/blocks/header.tsx
@@ -4,29 +4,29 @@ import React, { useEffect, useState } from "react"
 import AnimatedButton from "@/components/ui/AnimatedButton"
 import Marquee from "../ui/Marquee"
 import Link from "next/link"
-import Lenis from "@studio-freight/lenis"
+// import Lenis from "@studio-freight/lenis"
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/Dialog"
 import ContactForm from "@/components/ui/Contact"
 
 const Header = () => {
     const [isDialogOpen, setIsDialogOpen] = useState(false)
 
-    useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            smooth: true,
-        })
+    // useEffect(() => {
+    //     const lenis = new Lenis({
+    //         duration: 1.2,
+    //         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    //         smooth: true,
+    //     })
 
-        function raf(time) {
-            lenis.raf(time)
-            requestAnimationFrame(raf)
-        }
+    //     function raf(time) {
+    //         lenis.raf(time)
+    //         requestAnimationFrame(raf)
+    //     }
 
-        requestAnimationFrame(raf)
+    //     requestAnimationFrame(raf)
 
-        return () => lenis.destroy()
-    }, [])
+    //     return () => lenis.destroy()
+    // }, [])
 
     const handleButtonClick = () => {
         setIsDialogOpen(true) // Open the dialog when button is clicked
